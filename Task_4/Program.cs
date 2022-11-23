45fb476250d586e780cffb7b38e4061888d06c66
Michael <m.zamchalin@yandex.ru>
2022-11-23 21:24:03 +0300

    Added Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите пожалуйста  число: ");
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= a)
+if (i % 2 == 0)
+{
+    Console.Write($"{i} ");
+    i++;
+}
+else {
+    i++;
+}
