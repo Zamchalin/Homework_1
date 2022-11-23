87add74d0daf857991c4c1591df65d636fc06d01
Michael <m.zamchalin@yandex.ru>
2022-11-23 20:58:13 +0300

    added task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите пожалуйста  число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+     Console.Write("Это число чётное.");
+else 
+    Console.Write("Это число нечётное.");
